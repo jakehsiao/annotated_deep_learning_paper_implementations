300c3d658148659f44c89e2ad83ffc5014f76477
隔壁的泰山233 <1499975101@qq.com>
2023-02-16 04:20:19 +0800

    Add activation for time embedding and dropout.

--- labml_nn/diffusion/ddpm/unet.py
+++ labml_nn/diffusion/ddpm/unet.py
@@ -91,18 +91,20 @@ class ResidualBlock(Module):
     Each resolution is processed with two residual blocks.
     """
 
-    def __init__(self, in_channels: int, out_channels: int, time_channels: int, n_groups: int = 32):
+    def __init__(self, in_channels: int, out_channels: int, time_channels: int, n_groups: int = 32, dropout_rate: float = 0.1):
         """
         * `in_channels` is the number of input channels
         * `out_channels` is the number of input channels
         * `time_channels` is the number channels in the time step ($t$) embeddings
         * `n_groups` is the number of groups for [group normalization](../../normalization/group_norm/index.html)
+        * `dropout_rate` is the dropout rate
         """
         super().__init__()
         # Group normalization and the first convolution layer
         self.norm1 = nn.GroupNorm(n_groups, in_channels)
         self.act1 = Swish()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), padding=(1, 1))
+        self.dropout_rate = dropout_rate
 
         # Group normalization and the second convolution layer
         self.norm2 = nn.GroupNorm(n_groups, out_channels)
@@ -118,6 +120,7 @@ class ResidualBlock(Module):
 
         # Linear layer for time embeddings
         self.time_emb = nn.Linear(time_channels, out_channels)
+        self.time_act = Swish()
 
     def forward(self, x: torch.Tensor, t: torch.Tensor):
         """
@@ -127,9 +130,9 @@ class ResidualBlock(Module):
         # First convolution layer
         h = self.conv1(self.act1(self.norm1(x)))
         # Add time embeddings
-        h += self.time_emb(t)[:, :, None, None]
+        h += self.time_emb(self.time_act(t))[:, :, None, None]
         # Second convolution layer
-        h = self.conv2(self.act2(self.norm2(h)))
+        h = self.conv2(F.dropout(self.act2(self.norm2(h)), self.dropout_rate))
 
         # Add the shortcut connection and return
         return h + self.shortcut(x)
